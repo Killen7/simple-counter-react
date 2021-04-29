d41933410fdf284162835b6a225e42f32baf8692
Nicolas Guillen <nicomounstro@gmail.com>
2021-04-29 19:30:16 +0000

    ejercicio terminado

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,46 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import PropTypes from "prop-types";
 
 //create your first component
-export function Home() {
+export function Home(props) {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center mt-5 d-flex justify-content-center bg-dark p-2 caja mx-auto rounded-lg">
+			<div className="mr-2 ml-2 cajita p-2">
+				<h1>
+					<i className="far fa-clock"></i>
+				</h1>
+			</div>
+			<div className="mr-2 cajita p-2">
+				<h1>{props.septimo}</h1>
+			</div>
+			<div className="mr-2 cajita p-2">
+				<h1>{props.sexto}</h1>
+			</div>
+			<div className="mr-2 cajita p-2">
+				<h1>{props.quinto}</h1>
+			</div>
+			<div className="mr-2 cajita p-2">
+				<h1>{props.cuarto}</h1>
+			</div>
+			<div className="mr-2 cajita p-2">
+				<h1>{props.tercero}</h1>
+			</div>
+			<div className="mr-2 cajita p-2">
+				<h1>{props.segundo}</h1>
+			</div>
+			<div className="mr-2 cajita p-2">
+				<h1>{props.primero}</h1>
+			</div>
 		</div>
 	);
 }
+
+Home.propTypes = {
+	primero: PropTypes.number,
+	segundo: PropTypes.number,
+	tercero: PropTypes.number,
+	cuarto: PropTypes.number,
+	quinto: PropTypes.number,
+	sexto: PropTypes.number,
+	septimo: PropTypes.number
+};
--- src/js/index.js
+++ src/js/index.js
@@ -12,4 +12,30 @@ import "../styles/index.scss";
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let contador = 0;
+
+setInterval(() => {
+	let primero = contador % 10;
+	let segundo = Math.floor(contador / 10) % 10;
+	let tercero = Math.floor(contador / 100) % 10;
+	let cuarto = Math.floor(contador / 1000) % 10;
+	let quinto = Math.floor(contador / 10000) % 10;
+	let sexto = Math.floor(contador / 100000) % 10;
+	let septimo = Math.floor(contador / 1000000) % 10;
+
+	contador++;
+	console.log(contador);
+
+	ReactDOM.render(
+		<Home
+			primero={primero}
+			segundo={segundo}
+			tercero={tercero}
+			cuarto={cuarto}
+			quinto={quinto}
+			sexto={sexto}
+			septimo={septimo}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
